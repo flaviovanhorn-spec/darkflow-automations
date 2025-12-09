f6c622ac3c82d745f8d7d6873a25d4abd7d11341
Flavio van Horn <flaviovanhorn@gmail.com>
2025-12-10 00:44:51 +0100

    Fix Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   //  #IMPORTANT for GitHub Pages
-  base: mode === "production" ? "/darkflow-automations-i18n/" : "/",
+  base: mode === "production" ? "/darkflow-automations/" : "/",
   server: {
     host: "::",
     port: 8080,
